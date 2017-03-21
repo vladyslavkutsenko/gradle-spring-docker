d7b8b1b4ac75d3add2988ec3f120f9cc103ed7bf
Vladyslav Kutsenko <vladyslav.kutsenko@itas-gmbh.de>
2017-03-21 14:11:24 +0100

    Enable component scan

--- src/main/java/de/itasgmbh/workshop/GatewayApplication.java
+++ src/main/java/de/itasgmbh/workshop/GatewayApplication.java
@@ -2,8 +2,11 @@ package de.itasgmbh.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "de.itasgmbh.workshop.gateway")
 public class GatewayApplication {
 
 	public static void main(String[] args) {
